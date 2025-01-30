41ec0c22cd7ca3d0fd295d7f0161adf9ef655dd2
Aiken Gunay <aikengunay.developer@gmail.com>
2025-01-30 15:19:57 +0800

    study: Section 007 - 005 What's the DOM and DOM Manipulation

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -2,3 +2,20 @@
 
 // read the text content inside of the class message
 console.log(document.querySelector('.message').textContent);
+
+// change text content to other
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+// display the text content
+console.log(document.querySelector('.message').textContent);
+
+// set the text content of these class
+document.querySelector('.number').textContent = 13;
+
+document.querySelector('.score').textContent = 10;
+
+// Set the value content to 50
+document.querySelector('.guess').value = 50;
+
+// display the value of guess input
+console.log(document.querySelector('.guess').value);
